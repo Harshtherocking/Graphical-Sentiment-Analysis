1f6ecf8dd9fc1dad1bedc52d81bbcfdcf6412cae
Harsh <harsh.thunder.happy@gmail.com>
2024-06-12 16:40:52 +0530

    forward method updated

--- MessagePassing/gcn.py
+++ MessagePassing/gcn.py
@@ -7,15 +7,14 @@ from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_se
 
 
 class myGCNConv (MessagePassing):
-    def __init__ (self, in_channel: int, out_channel: int) : 
+    def __init__ (self, in_channel: int, out_channel: int, attr_in_channel : int) : 
         super().__init__(aggr= "mean") 
         
         self.out_channel = out_channel
 
-        # shape = (out_channel, in_channel)
         self.A= Linear(in_channel, out_channel, bias = False)
 
-        self.B= Linear(in_features= edge_attr.shape[1], out_features= self.out_channel, bias = False)
+        self.B= Linear(in_features= attr_in_channel, out_features= self.out_channel, bias = False)
 
         self.bias = Parameter(torch.empty(out_channel))
         
@@ -25,10 +24,15 @@ class myGCNConv (MessagePassing):
         self.bias.data.zero_()
         return super().reset_parameters()
 
-    def forward(self, x , edge_index, edge_attr ) : 
 
+    def forward(self, G : Data) : 
+        x = G["x"]
+        edge_attr =  G["edge_attr"]
+        edge_index = G["edge_index"]
+        
         # check for isolated nodes 
-        assert not contains_isolated_nodes(edge_index, x.shape[0]), "graph has isolated nodes" 
+        # assert not contains_isolated_nodes(edge_index, x.shape[0]), "graph has isolated nodes" 
+        assert edge_index.shape[1] == edge_attr.shape[0], f"Edges : {edge_index.shape[1]}, Attrs : {edge_attr.shape[0]}"
 
         # adding self-loops
         edge_index, edge_attr = add_remaining_self_loops(
@@ -64,15 +68,13 @@ class myGCNConv (MessagePassing):
 
 # ---------------------------------------------------------
 class GcnDenseModel (Module):
-    def __init__(self, input_feature_size : int , output_feature_size : int, hid_size : int):
-        super().__init__(self)
+    def __init__(self, input_feature_size : int , output_feature_size : int, hid_size : int, dep_feature_size : int):
+        super().__init__()
 
         self.hid_size = hid_size
-        self.c0 = torch.randn(3,self.hid_size, requires_grad=True)
-        self.h0 = torch.randn(3,self.hid_size, requires_grad=True)
 
         # graph convolution layer 
-        self.gcn = myGCNConv(input_feature_size, output_feature_size)
+        self.gcn = myGCNConv(input_feature_size, output_feature_size,dep_feature_size)
         self.sigmoid = Sigmoid()
 
         self.lstm = LSTM(input_size=output_feature_size, hidden_size=self.hid_size, bias = False, num_layers=3, batch_first= True)
@@ -80,26 +82,24 @@ class GcnDenseModel (Module):
         self.max_pool = AdaptiveMaxPool1d(1)
         self.avg_pool = AdaptiveAvgPool1d(1)
 
-        self.lin = Linear(out_features = 3, in_features= 3*self.hid_size)
+        self.lin = Linear(out_features = 3, in_features= 3*self.hid_size + 2*hid_size)
         self.softmax = Softmax(dim=1)
 
         return None
 
 
-    def forward (self, graph: list[Data]): 
-        batch_size = len(graph)
-        self.x = [g["x"] for g in graph]
-        self.edge_attr = [g["edge_attr"] for g in graph]
-        edge_index = [g["edge_index"] for g in graph]
-        
-        # graph convolution on each graph in the batch
+    def forward (self, graphs: list[Data]): 
+        batch_size = len(graphs)
+        self.init_states(batch_size)
         seq = []
         lengths = []
-        for idx in range(batch_size):
-            node = self.gcn(self.x[idx], self.edge_attr[idx], edge_index[idx])
+
+        # graph convolution on each graph in the batch
+        for g in graphs : 
+            node = self.gcn(g)
             seq.append(self.sigmoid(node))
             lengths.append(node.shape[0])
-
+            
         # padding of node list : x(i) has shape (length(i), out_features)
         padded_seq = pad_sequence(seq, batch_first = True)
 
@@ -111,17 +111,22 @@ class GcnDenseModel (Module):
         h_t, c_t  = states
 
         # pooling the output
-        lstm_out, lengths = pad_packed_sequence(lstm_out)
+        lstm_out, lengths = pad_packed_sequence(lstm_out, batch_first = True)
         max_pool = self.max_pool(lstm_out.permute(0,2,1)).view(batch_size,-1)
         avg_pool = self.avg_pool(lstm_out.permute(0,2,1)).view(batch_size,-1)
 
         # concat pooling
-        pool_concat = torch.concat([h_t[-1],max_pool,avg_pool])
+        pool_concat = torch.concat([h_t.view(batch_size,-1),max_pool,avg_pool], dim=1)
         
         # dense layer 
         res = self.lin(pool_concat)
         return self.softmax(res)
-        
+
+    def init_states (self, batch_size):
+        self.c0 = torch.randn(3,batch_size,self.hid_size, requires_grad=True)   
+        self.h0 = torch.randn(3,batch_size,self.hid_size, requires_grad=True)
+        return None
+
 
 # ------------------------------------------------------------------------
 
@@ -131,3 +136,7 @@ if __name__ == "__main__":
     x = graph["x"]
     edge_attr = graph["edge_attr"]
     edge_index = graph["edge_index"]
+
+    gnn = myGCNConv(in_channel =96, out_channel= 16, attr_in_channel =16)
+    x = gnn (x,edge_index,edge_attr)
+    print(x.shape)
